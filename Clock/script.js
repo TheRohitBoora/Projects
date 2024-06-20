ce9226a90145d4e675491f8d45c9907cac607390
Rohit <rohitboora8080@gmail.com>
2024-06-21 00:40:58 +0530

    Update script.js

--- Clock/script.js
+++ Clock/script.js
@@ -23,18 +23,14 @@ function clock() {
 
 
     const period = hours >= 12 ? "PM" : "AM";
-    if (hours > 12) {
-        hours = hours - 12
+    if (hours === 0) {
+        hours = 12; 
+    } else if (hours > 12) {
+        hours -= 12; 
     }
-    if (hours < 10) {
-        hours = "0" + hours
-    }
-    if (minutes < 10) {
-        minutes = "0" + minutes
-    }
-    if (seconds < 10) {
-        seconds = "0" + seconds
-    }
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
     time.innerHTML = `${hours}:${minutes}:${seconds} ${period} `
     wday.innerHTML = `${day} <br> `
     dates.innerHTML = `${dvalue} ${month}, ${year}`
